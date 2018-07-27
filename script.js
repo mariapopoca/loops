213d6a16de93db5b029451cf70386712f6d8bf2b
DePaul <CONF0206@depaul.edu>
2018-07-27 15:34:36 -0500

    songs

---
+++ script.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){ 
+
+	console.log("Songs");
+	console.log(songs_array ["Jennifer","Warm on a Cold Night", "Crew love", "I do", "Drowning"]);
+	songs.arroy.pop();
+	
+	
+
+
+
+
+
+
+
+
+
+
+
+	
+	});
